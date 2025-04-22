a5f427cbffde4a2ca55110a79de82be3ec1ede7c
Alblak <albertkambale16@gmail.com>
2025-04-22 20:59:20 +0200

    add script js

---
+++ include/script.php
@@ -0,0 +1,18 @@
+<script src="../assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="../assets/vendor/aos/aos.js"></script>
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../assets/vendor/typed.js/typed.umd.js"></script>
+  <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  
+  <script src="../assets/vendor/quill/quill.min.js"></script>
+  <script src="../assets/js/simple-datatables/simple-datatables.js"></script>
+  <script src="../assets/js/tinymce/tinymce.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+
+
+  <!-- Template Main JS File -->
+  <script src="../assets/js/main.js"></script>
