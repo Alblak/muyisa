7446c1eabf68b2469595a04a9cdd20537ba6ee3c
Alblak <albertkambale16@gmail.com>
2025-04-22 20:58:08 +0200

    add footers

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+
+
+<div class="container ">
+      <div class="copyright">
+        &copy; Copyright <strong><span>MUYISA ÉNERGIE</span></strong>
+      </div>
+      <div class="credits">
+       
+        Designed by <a href="">Albert</a>
+      </div>
+</div>
