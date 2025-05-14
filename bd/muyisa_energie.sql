19164c0f07f06aaae05e706b44aeb3c37ecf2936
Alblak <albertkambale16@gmail.com>
2025-05-14 17:55:58 +0200

    update database

--- bd/muyisa_energie.sql
+++ bd/muyisa_energie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 09 mai 2025 à 08:41
+-- Généré le : mer. 14 mai 2025 à 17:54
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.0.30
 
@@ -24,26 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `approvisionnement`
+-- Structure de la table `bondesortie`
 --
 
-CREATE TABLE `approvisionnement` (
+CREATE TABLE `bondesortie` (
   `id` int(11) NOT NULL,
   `dates` date NOT NULL,
-  `type` varchar(11) NOT NULL,
-  `quantite` double NOT NULL,
+  `libelle` text NOT NULL,
   `montant` double NOT NULL,
-  `statut` int(11) NOT NULL,
   `supprimer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `approvisionnement`
+-- Déchargement des données de la table `bondesortie`
 --
 
-INSERT INTO `approvisionnement` (`id`, `dates`, `type`, `quantite`, `montant`, `statut`, `supprimer`) VALUES
-(2, '2025-04-23', 'mazout', 12000, 2, 0, 0),
-(3, '2025-04-24', 'essence', 20000, 200000000, 0, 0);
+INSERT INTO `bondesortie` (`id`, `dates`, `libelle`, `montant`, `supprimer`) VALUES
+(1, '2025-05-13', 'achat materiel', 150, 0),
+(2, '2025-05-13', 'frais de transport', 200, 1),
+(3, '2025-05-13', 'payement logiciel', 88, 0);
 
 -- --------------------------------------------------------
 
@@ -93,14 +92,16 @@ CREATE TABLE `commande` (
 --
 
 INSERT INTO `commande` (`id`, `dates`, `client`, `type`, `numfacture`, `statut`, `supprimer`) VALUES
-(8, '2025-04-26', 'ME0001', 0, 1, 1, 0),
-(9, '2025-04-26', 'ME0002', 0, 2, 1, 0),
-(10, '2025-04-28', 'ME0002', 0, 3, 0, 0),
-(11, '2025-04-28', 'ME0001', 0, 4, 0, 0),
-(12, '2025-04-28', 'ME0001', 0, 5, 1, 0),
-(13, '2025-04-28', 'ME0001', 0, 6, 0, 0),
-(14, '2025-04-28', 'ME0001', 0, 7, 1, 0),
-(15, '2025-05-06', 'ME0002', 0, 8, 0, 0);
+(8, '2025-04-26', 'ME0001', 2, 1, 1, 0),
+(9, '2025-04-26', 'ME0002', 2, 2, 1, 0),
+(12, '2025-04-28', 'ME0001', 2, 5, 1, 0),
+(14, '2025-04-28', 'ME0001', 2, 7, 1, 0),
+(19, '2025-05-09', 'ME0002', 2, 12, 1, 0),
+(20, '2025-05-09', 'ME0002', 1, 13, 1, 0),
+(22, '2025-05-14', 'ME0002', 2, 14, 0, 0),
+(23, '2025-05-14', 'ME0002', 1, 15, 1, 0),
+(24, '2025-05-14', 'ME0002', 1, 16, 0, 0),
+(25, '2025-05-14', 'ME0002', 1, 17, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -184,6 +185,60 @@ INSERT INTO `fournisseur` (`id`, `nom`, `postnom`, `prenom`, `adresse`, `telepho
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `mois`
+--
+
+CREATE TABLE `mois` (
+  `id` int(11) NOT NULL,
+  `mois` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `mois`
+--
+
+INSERT INTO `mois` (`id`, `mois`) VALUES
+(1, 'janvier'),
+(2, 'fevrier'),
+(3, 'mars'),
+(4, 'avril'),
+(5, 'mai'),
+(6, 'juin'),
+(7, 'juillet'),
+(8, 'Aout'),
+(9, 'septembre'),
+(10, 'octobre'),
+(11, 'novembre'),
+(12, 'decembre');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paiment_dette`
+--
+
+CREATE TABLE `paiment_dette` (
+  `id` int(11) NOT NULL,
+  `dates` date NOT NULL,
+  `commande` int(11) NOT NULL,
+  `montant` double NOT NULL,
+  `supprimer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `paiment_dette`
+--
+
+INSERT INTO `paiment_dette` (`id`, `dates`, `commande`, `montant`, `supprimer`) VALUES
+(1, '2025-05-14', 9, 23, 0),
+(2, '2025-05-14', 9, 20000, 0),
+(3, '2025-05-14', 9, 254477, 0),
+(4, '2025-05-14', 12, 500, 0),
+(5, '2025-05-14', 19, 336000, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `panier`
 --
 
@@ -214,7 +269,15 @@ INSERT INTO `panier` (`id`, `commande`, `type`, `type_achat`, `quantite`, `prixu
 (14, 2, 'essence', '', 22, 80000, 0),
 (15, 3, 'essence', '', 12, 80000, 0),
 (16, 4, 'mazout', '', 4, 300000, 0),
-(17, 5, 'essence', '', 12, 80000, 0);
+(17, 5, 'essence', '', 12, 80000, 0),
+(18, 19, 'mazout', 'litre', 56, 6000, 0),
+(19, 20, 'essence', 'litre', 23, 1, 0),
+(21, 20, 'essence', 'fut', 2, 200, 0),
+(22, 20, 'mazout', 'fut', 0.5, 250, 0),
+(23, 22, 'essence', 'fut', 1, 200, 0),
+(24, 23, 'essence', 'fut', 200, 200, 0),
+(25, 24, 'essence', 'fut', 1, 200, 0),
+(26, 25, 'essence', 'fut', 1, 200, 0);
 
 -- --------------------------------------------------------
 
@@ -248,6 +311,34 @@ INSERT INTO `panier_ap` (`id`, `type`, `commande`, `quantite`, `prixunitaire`, `
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `personnel`
+--
+
+CREATE TABLE `personnel` (
+  `matricule` varchar(15) NOT NULL,
+  `nom` text NOT NULL,
+  `postnom` text NOT NULL,
+  `prenom` text NOT NULL,
+  `photo` text NOT NULL,
+  `genre` varchar(1) NOT NULL,
+  `telephone` text NOT NULL,
+  `fonction` text NOT NULL,
+  `salaire` double NOT NULL,
+  `date_embauche` date NOT NULL,
+  `supprimer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `personnel`
+--
+
+INSERT INTO `personnel` (`matricule`, `nom`, `postnom`, `prenom`, `photo`, `genre`, `telephone`, `fonction`, `salaire`, `date_embauche`, `supprimer`) VALUES
+('AG-0001', 'kambale', 'kamala', 'albert', '1746983396.jpg', 'M', '0977139499', 'gerant', 200, '2025-05-11', 0),
+('AG-0002', 'kambale', 'Kilima', 'julien', '1747049990.jpg', 'M', '0977134499', 'comptable', 150, '2025-05-12', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `prix`
 --
 
@@ -269,7 +360,41 @@ INSERT INTO `prix` (`id`, `dates`, `prix_essenceL`, `prix_mazoutL`, `prix_essenc
 (1, '2025-04-24', 5000, 6000, 0, 0, 0),
 (2, '2025-04-24', 5500, 6000, 0, 0, 0),
 (3, '2025-04-24', 5500, 6000, 80000, 840999, 0),
-(4, '2025-04-28', 5500, 6000, 80000, 300000, 0);
+(4, '2025-04-28', 5500, 6000, 80000, 300000, 0),
+(5, '2025-05-09', 1, 1.2, 200, 250, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `remuneration`
+--
+
+CREATE TABLE `remuneration` (
+  `id` int(11) NOT NULL,
+  `dates` date NOT NULL,
+  `personnel` text NOT NULL,
+  `montant` double NOT NULL,
+  `mois` int(11) NOT NULL,
+  `annee` year(4) NOT NULL,
+  `supprimer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `remuneration`
+--
+
+INSERT INTO `remuneration` (`id`, `dates`, `personnel`, `montant`, `mois`, `annee`, `supprimer`) VALUES
+(1, '2025-05-12', 'AG-0001', 40, 1, '2025', 0),
+(2, '2025-05-12', 'AG-0001', 150, 1, '2025', 0),
+(3, '2025-05-12', 'AG-0001', 8, 1, '2025', 0),
+(4, '2025-05-12', 'AG-0001', 2, 1, '2025', 0),
+(5, '2025-05-12', 'AG-0002', 150, 1, '2025', 0),
+(6, '2025-05-12', 'AG-0002', 150, 2, '2025', 0),
+(7, '2025-05-12', 'AG-0002', 150, 3, '2025', 0),
+(8, '2025-05-12', 'AG-0002', 150, 4, '2025', 0),
+(9, '2025-05-12', 'AG-0002', 50, 5, '2025', 0),
+(11, '2025-05-12', 'AG-0001', 200, 2, '2025', 0),
+(12, '2025-05-12', 'AG-0001', 100, 3, '2025', 0);
 
 -- --------------------------------------------------------
 
@@ -335,16 +460,17 @@ CREATE TABLE `utilisateur` (
 INSERT INTO `utilisateur` (`id`, `nom`, `postnom`, `prenom`, `fonction`, `photo`, `username`, `password`, `supprimer`) VALUES
 (1, 'kambale', 'kiLIM', 'julien', 'caissiere', '1745823599.jpg', 'julienkiLIM.@me.drc', '0101', 0),
 (2, 'kmk', 'kkk', 'klkk', 'admin', 'sscru3.webp', 'klkkkkk.@me.drc', '8898', 0),
-(3, 'kambale', 'kamala', 'albert', 'gerant', '1745404332.jpg', 'albertkamala.@me.drc', '0101', 0);
+(3, 'kambale', 'kamala', 'albert', 'gerant', '1745404332.jpg', 'albertkamala.@me.drc', '0101', 0),
+(4, 'muhindo', 'kombi', 'jospin', 'comptable', '1747163126.jpg', 'jospinkombi.@me.drc', '0101', 0);
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `approvisionnement`
+-- Index pour la table `bondesortie`
 --
-ALTER TABLE `approvisionnement`
+ALTER TABLE `bondesortie`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -378,6 +504,18 @@ ALTER TABLE `fournisseur`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `mois`
+--
+ALTER TABLE `mois`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `paiment_dette`
+--
+ALTER TABLE `paiment_dette`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `panier`
 --
 ALTER TABLE `panier`
@@ -390,9 +528,21 @@ ALTER TABLE `panier_ap`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `personnel`
+--
+ALTER TABLE `personnel`
+  ADD PRIMARY KEY (`matricule`);
+
+--
 -- Index pour la table `prix`
 --
 ALTER TABLE `prix`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `remuneration`
+--
+ALTER TABLE `remuneration`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -418,16 +568,16 @@ ALTER TABLE `utilisateur`
 --
 
 --
--- AUTO_INCREMENT pour la table `approvisionnement`
+-- AUTO_INCREMENT pour la table `bondesortie`
 --
-ALTER TABLE `approvisionnement`
+ALTER TABLE `bondesortie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `commande`
 --
 ALTER TABLE `commande`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT pour la table `commande_ap`
@@ -448,10 +598,22 @@ ALTER TABLE `fournisseur`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT pour la table `mois`
+--
+ALTER TABLE `mois`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT pour la table `paiment_dette`
+--
+ALTER TABLE `paiment_dette`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT pour la table `panier`
 --
 ALTER TABLE `panier`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT pour la table `panier_ap`
@@ -463,7 +625,13 @@ ALTER TABLE `panier_ap`
 -- AUTO_INCREMENT pour la table `prix`
 --
 ALTER TABLE `prix`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `remuneration`
+--
+ALTER TABLE `remuneration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `sortie`
@@ -481,7 +649,7 @@ ALTER TABLE `taux`
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
